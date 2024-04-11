9e99e809c1260839bbcb20f33d1beeae59394408
i.filippov <ivan.filippov@sigur.com>
2024-04-11 14:29:47 +0300

    Updates ft_cp.py script. Adds binary file read script

--- binary_test.py
+++ binary_test.py
@@ -1,44 +1,13 @@
-from osdp import *
+"""
+Binary file read
+"""
+
 import os
-
-conn = SerialPortOsdpConnection(port="COM9", baud_rate=9600)
-cp = ControlPanel()
-bus_id = cp.start_connection(conn)
-cp.add_device(
-    connection_id=bus_id, address=0x02, use_crc=False, use_secure_channel=False
-)
-id_report = cp.id_report(connection_id=bus_id, address=0x02)
-device_capabilities = cp.device_capabilities(connection_id=bus_id, address=0x02)
-local_status = cp.local_status(connection_id=bus_id, address=0x02)
-input_status = cp.input_status(connection_id=bus_id, address=0x02)
-output_status = cp.output_status(connection_id=bus_id, address=0x02)
-
-
-class FileTransferTestCommand(Command):
-    def __init__(self, address: int, manufacturer_data: bytes):
-        self.address = address
-        self.manufacturer_data = manufacturer_data
-
-    @property
-    def command_code(self) -> int:
-        return 0x7C
-
-    def security_control_block(self) -> bytes:
-        return bytes([0x02, 0x17])
-
-    def data(self) -> bytes:
-        return self.manufacturer_data
-
-    def custom_command_update(self, command_buffer: bytearray):
-        pass
-
-
-device = Device(address=0x02, use_crc=False, use_secure_channel=False)
 
 FIRST_CHUNK_SIZE: int = 44
 CHUNK_SIZE: int = 1421
 CMD_TYPE: list[int] = [0x01]
-FILE_NAME: str = "test.mcu"
+FILE_NAME: str = 'test.mcu'
 
 file_size = os.path.getsize(FILE_NAME)
 total_size: list[int] = list(file_size.to_bytes(4, "little"))
@@ -61,17 +30,12 @@ with open(FILE_NAME, "rb") as file:
     test_ft_cmd = bytes(
         CMD_TYPE + total_size + offset_bytes + initial_fragment_size + chunk_data_list
     )
-    file_transfer_command = FileTransferTestCommand(
-        address=0x02, manufacturer_data=test_ft_cmd
-    )
-    file_transfer_command.build_command(device)
-    cp.send_custom_command(connection_id=bus_id, command=file_transfer_command)
 
     while chunk_data:  # loop until the chunk is empty (the file is exhausted)
         print("WHILE LOOP")
 
         chunk_data = bytearray(file.read(CHUNK_SIZE))  # read the next chunk
-        print(f"Bytearray test data is {chunk_data}")
+        # print(f"Bytearray test data is {chunk_data}")
 
         chunk_data_list: list[int] = list(chunk_data)
         print(f"CHUNK DATA LIST is {chunk_data_list}")
@@ -100,10 +64,5 @@ with open(FILE_NAME, "rb") as file:
         test_ft_cmd = bytes(
             CMD_TYPE + total_size + offset_bytes + fragment_size + chunk_data_list
         )
-        file_transfer_command = FileTransferTestCommand(
-            address=0x02, manufacturer_data=test_ft_cmd
-        )
-        file_transfer_command.build_command(device)
-        cp.send_custom_command(connection_id=bus_id, command=file_transfer_command)
+        # print(f"CMD {test_ft_cmd}")
 file.close()
-cp.shutdown()
